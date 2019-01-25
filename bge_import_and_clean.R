0dd1415d3ca8286f7f51cda2e4be01257717c7dd
Jonathan Bost <jwebost@gmail.com>
2019-01-25 11:57:05 -0500

    changed some logs and added code to remove stop words from the cleanedwords vector

--- bge_import_and_clean.R
+++ bge_import_and_clean.R
@@ -11,6 +11,7 @@ library(stringr)
 library(rvest)
 library(XML)
 library(tidyr)
+library(tm)
 
 
 # IMPORTING
@@ -27,7 +28,7 @@ bge <- webpage %>% html_nodes("p") %>% html_text()
 
 bge <- iconv(bge, "latin1", "ASCII", sub="")
 
-print("imported")
+print("import finished.")
 
 
 #CLEANING 
@@ -35,6 +36,7 @@ print("imported")
 #bge is full text, cutting down to just apophthegms
 #known badness here is that i hardcoded the start and end. 
 # this should be abstracted to a function that takes url, start and end strings as inputs and cuts down to that
+
 apophthegms <- bge[83:207]
 
 #each sentence begins w/ "\n    " so gonna take that out, take out punctuation and digits and whitespace
@@ -54,9 +56,20 @@ cleanedtext <- apophthegms
 
 #create cleaned words and then remove stop words
 
-cleanedwords <- unlist(strsplit(apophthegms,split = " "))
+stopwords_regex = paste(stopwords('en'), collapse = '\\b|\\b')
+stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
 
-#cleanedwords <- 
+cleanedwords <- unlist(strsplit(apophthegms,split = " "))
+cleanedwords <- str_replace_all(cleanedwords,stopwords_regex,'') 
+
+wordcountbefore <- length(cleanedwords)
+
+cleanedwords <- cleanedwords[cleanedwords != '']
+
+wordcountafter <- length(cleanedwords)
+pctremoved <- round(((wordcountbefore-wordcountafter)/wordcountbefore)*100,1)
+
+cat(wordcountbefore-wordcountafter," words (",pctremoved,"%) were removed. ",wordcountafter," words remain.")
 
 # the lemmatization function requires the text to be in a .txt file encoded in UTF-8
 
@@ -65,7 +78,7 @@ Encoding(cleanedtext) <- "UTF-8"
 write.table(cleanedtext, file = "cleanedtext.txt",sep = "")
 
 
-print("cleaned")
+print("text has been cleaned and saved as cleanedtext.txt")
 
 #measure time it took 
 
