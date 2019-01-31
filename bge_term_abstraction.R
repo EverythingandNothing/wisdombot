d329fe93a70949223db64c41f29b6148aa500d76
Jonathan Bost <jwebost@gmail.com>
2019-01-31 14:16:07 -0500

    using NLP/open NLP to get POS tagging. method currently returns an error that needs debugging but basic POS tagging works, so saving as it's promising.

--- bge_term_abstraction.R
+++ bge_term_abstraction.R
@@ -8,9 +8,17 @@ print("starting term abstraction")
 
 
 #open libraries
-library(textstem)
+library(NLP)
+library(openNLP)
+library(openNLPmodels.en)
+library(tm)
+library(stringr)
+library(gsubfn)
+library(plyr)
 
-# ATTEMPT TO USE TREETAG FAILED, USING OTHER METHODS UNTIL THEN
+
+# ATTEMPT USING TREETAG; failed because it couldn't find a file called englishlexicon.txt
+# i looked for this file and asked on stack overflow but couldn't figure it out so tried other methods
 #
 #library(koRpus)
 # 
@@ -25,8 +33,30 @@ library(textstem)
 #                                            TT.options=list(path="/Applications/TreeTagger", preset="en"))
 # 
 
+#ATTEMPT USING NLP/openNLP from http://martinschweinberger.de/docs/articles/PosTagR.pdf 
+listtext <- list("text" = cleanedtext)
+
+texttagged <- sapply(listtext, function(x){
+   sent_token_annotator <- Maxent_Sent_Token_Annotator()
+   word_token_annotator <- Maxent_Word_Token_Annotator()
+   pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+   y1 <- annotate (x,list(sent_token_annotator,word_token_annotator))
+   y2 <- annotate(x, pos_tag_annotator, y1)
+   # y3 <- annotate (x, Maxent_POS_Tag_Annotator ( probs = TRUE ),y1)
+ y2w <- subset(y2, type == "word")
+ tags <- sapply(y2w$features , '[[', "POS")
+ r1 <- sprintf("%s/%s", x[y2w] , tags)
+ r2 <- paste (r1, collapse = " ")
+ return (r2)})
 
 
 
-leammatizedtext <- lemmatize_words(cleanedtext)
+
+
+
+
+
+
+
+
 
