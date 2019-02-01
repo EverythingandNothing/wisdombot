f86561402a10b1580aa225f6559b90ef67525bbb
Jonathan Bost <jwebost@gmail.com>
2019-02-01 16:56:55 -0500

    stood up basic POS tagging. two output - 1 is a list of two items; all aphorisms and the POS structure of the aphorisms. 1 is a list of all words and the POS of those words
    added and imported a csv that describes the POS for my reference.

--- bge_term_abstraction.R
+++ bge_term_abstraction.R
@@ -7,7 +7,9 @@ start<-Sys.time()
 print("starting term abstraction")
 
 
-#open libraries
+#open libraries and import csv of POS descriptions
+print("opening libraries and importing POS Descriptions")
+
 library(NLP)
 library(openNLP)
 library(openNLPmodels.en)
@@ -15,6 +17,9 @@ library(tm)
 library(stringr)
 library(gsubfn)
 library(plyr)
+library(dplyr)
+
+POSdescriptions <- read.csv("POSDescription.csv", stringsAsFactors = FALSE)
 
 
 # ATTEMPT USING TREETAG; failed because it couldn't find a file called englishlexicon.txt
@@ -34,21 +39,54 @@ library(plyr)
 # 
 
 #ATTEMPT USING NLP/openNLP from http://martinschweinberger.de/docs/articles/PosTagR.pdf 
+# formats the cleaned text as a list: $text is the raw text, $POS is the POS
+print("tagging parts of speech")
+
 listtext <- list("text" = cleanedtext)
 
-texttagged <- sapply(listtext, function(x){
-   sent_token_annotator <- Maxent_Sent_Token_Annotator()
-   word_token_annotator <- Maxent_Word_Token_Annotator()
-   pos_tag_annotator <- Maxent_POS_Tag_Annotator()
-   y1 <- annotate (x,list(sent_token_annotator,word_token_annotator))
-   y2 <- annotate(x, pos_tag_annotator, y1)
-   # y3 <- annotate (x, Maxent_POS_Tag_Annotator ( probs = TRUE ),y1)
- y2w <- subset(y2, type == "word")
- tags <- sapply(y2w$features , '[[', "POS")
- r1 <- sprintf("%s/%s", x[y2w] , tags)
- r2 <- paste (r1, collapse = " ")
- return (r2)})
+#function takes a character vector as an input and outputs a list of the POS
+tagPOS <- function(x){
+  
+  y1 <- annotate (x,list(sent_token_annotator,word_token_annotator))
+  y2 <- annotate(x, pos_tag_annotator, y1)
+  y2w <- subset(y2, type == "word") 
+  tags <- list(sapply(y2w$features , '[[', "POS"))
+  return(tags)
+}
 
+#applies the tag function to the text 
+for (i in 1:length(listtext$text)){
+  listtext$POS[i] <- tag(listtext$text[[i]])
+}
+
+
+#creating a vector that holds all the words, remove blanks, join w POS and POS description
+
+words <- c()
+
+for(i in 1:length(listtext$text)){
+  
+  words <- c(words,unlist(strsplit(listtext$text[[i]],split=" ")))
+  
+}
+
+words <- words[words != ""]
+
+POS <- unlist(listtext$POS)
+
+dfwords <- data.frame("words" = words, "POS" = POS, stringsAsFactors = FALSE) 
+dfwords <- left_join(dfwords, POSdescriptions, by = "POS")
+
+
+
+
+#measure time it took 
+
+print("done.")
+
+end <- Sys.time()
+
+end - start
 
 
 
